99f22b8b70f24c2944db6feaa7abf022d79f25cd
Dmitry Zhukov <56310359+dmitryjima@users.noreply.github.com>
2026-09-22 09:20:26 -0500

    Add TimelineMap component

---
+++ src/lib/timeline/components/TimelineMap/index.tsx
@@ -0,0 +1,172 @@
+import React, { Fragment, useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { useMeasure } from "react-use";
+import styles from "./styles.module.scss";
+import classNames from "classnames/bind";
+import type { TimelineNode, TimelineNodeData, TimelineTick } from "../../engine/types";
+import { TimelineEngine } from "../../engine";
+import { bridgePathCurved, stemPath } from "../../engine/paths";
+import {
+  MAX_TIMELINE_HORIZONTAL_PADDING_PER_YEAR_PX,
+  MIN_TIMELINE_HORIZONTAL_PADDING_PER_YEAR_PX,
+  TIMELINE_HORIZONTAL_PADDING_PX,
+} from "../../engine/constants";
+import NodeCard from "../NodeCard";
+
+const cx = classNames.bind(styles);
+
+type Props = {
+  data: TimelineNodeData[];
+  onNodeExpand: (node: TimelineNode) => void;
+};
+
+const domainStart = new Date(2017, 0, 1);
+const domainEnd = new Date();
+const domainYears = domainEnd.getFullYear() - domainStart.getFullYear();
+const minContentWidth =
+  MIN_TIMELINE_HORIZONTAL_PADDING_PER_YEAR_PX * domainYears + TIMELINE_HORIZONTAL_PADDING_PX * 2;
+const maxContentWidth =
+  MAX_TIMELINE_HORIZONTAL_PADDING_PER_YEAR_PX * domainYears + TIMELINE_HORIZONTAL_PADDING_PX * 2;
+
+const TimelineMap: React.FC<Props> = ({ data, onNodeExpand }) => {
+  const [scrollElementRef, { width: scrollWrapperWidth, height: scrollWrapperHeight }] =
+    useMeasure<HTMLDivElement>();
+
+  const [nodes, setNodes] = useState<TimelineNode[]>([]);
+  const handleSetStatefulNodes = useCallback((updatedNodes: TimelineNode[]) => {
+    setNodes(updatedNodes);
+  }, []);
+
+  // Rendered width of the content:
+  // do not exceed the `maxContentWidth` constant, but choose the largest between the width of the scroll wrapper and `minContentWidth`
+  const contentWidth = useMemo(() => {
+    return Math.min(Math.max(scrollWrapperWidth, minContentWidth), maxContentWidth);
+  }, [scrollWrapperWidth]);
+
+  // Position of the baseline
+  const axisY = useMemo(() => scrollWrapperHeight / 2, [scrollWrapperHeight]);
+
+  const timelineEngineInstanceRef = useRef<TimelineEngine>(null);
+
+  const [yearTicks, setYearTicks] = useState<TimelineTick[]>([]);
+  const [monthTicks, setMonthTicks] = useState<TimelineTick[]>([]);
+
+  const handleUpdateTicks = useCallback((yearTicks: TimelineTick[], monthTicks: TimelineTick[]) => {
+    setYearTicks(yearTicks);
+    setMonthTicks(monthTicks);
+  }, []);
+
+  useEffect(() => {
+    if (!contentWidth || !axisY) {
+      return;
+    }
+
+    if (!timelineEngineInstanceRef.current) {
+      // Instantiate the TimelineEngine if not present
+      timelineEngineInstanceRef.current = new TimelineEngine({
+        data,
+        domainStart: new Date(2017, 0, 1),
+        domainEnd: new Date(),
+        width: contentWidth,
+        axisY,
+        handleSetStatefulNodes,
+        handleUpdateTicks,
+      });
+    } else {
+      // Run `resize` method on change
+      timelineEngineInstanceRef.current.resize(contentWidth, axisY);
+    }
+  }, [contentWidth, axisY, data, handleSetStatefulNodes, handleUpdateTicks]);
+
+  return (
+    <div className={styles["scroll-wrapper"]} ref={scrollElementRef}>
+      {contentWidth === 0 || scrollWrapperHeight === 0 ? null : (
+        <div className={styles["inner-content"]} style={{ width: contentWidth }}>
+          <svg
+            className={styles["svg-map"]}
+            style={{
+              width: contentWidth,
+              height: scrollWrapperHeight,
+            }}
+            aria-hidden={true}
+          >
+            {/* Baseline */}
+            <line
+              className={styles["baseline"]}
+              x1={0}
+              x2={contentWidth}
+              y1={scrollWrapperHeight / 2}
+              y2={scrollWrapperHeight / 2}
+            />
+
+            {/* Month ticks */}
+            {monthTicks.map((tick, i) => {
+              return (
+                <line
+                  key={i}
+                  className={cx(styles["month-tick"])}
+                  x1={tick.x}
+                  x2={tick.x}
+                  y1={scrollWrapperHeight / 2 - 4}
+                  y2={scrollWrapperHeight / 2 + 4}
+                />
+              );
+            })}
+
+            {/* Year ticks */}
+            {yearTicks.map((tick, i) => {
+              return (
+                <Fragment key={i}>
+                  <text className={styles["year-label"]} x={tick.x} y={18}>
+                    {tick.label}
+                  </text>
+                  <line
+                    className={styles["year-tick"]}
+                    x1={tick.x}
+                    x2={tick.x}
+                    y1={0}
+                    y2={scrollWrapperHeight}
+                  />
+                  <text className={styles["year-label"]} x={tick.x} y={scrollWrapperHeight - 12}>
+                    {tick.label}
+                  </text>
+                </Fragment>
+              );
+            })}
+
+            {/* Paths to the nodes */}
+            {nodes.map((node) => {
+              let pathData: string;
+
+              if (node.endX !== undefined) {
+                pathData = bridgePathCurved(node.startX, node.endX, node.y, axisY, {
+                  openEnded: node.ongoing,
+                });
+              } else {
+                pathData = stemPath(node.startX, node.y, axisY);
+              }
+
+              return (
+                <path
+                  key={node.id}
+                  className={cx(styles["connector"], {
+                    commitment: node.type === "commitment",
+                    project: node.type === "project",
+                  })}
+                  d={pathData}
+                  fill="none"
+                />
+              );
+            })}
+          </svg>
+
+          {/* Node cards */}
+          {nodes.map((node) => {
+            return <NodeCard key={node.id} node={node} onExpand={onNodeExpand} />;
+          })}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default TimelineMap;
