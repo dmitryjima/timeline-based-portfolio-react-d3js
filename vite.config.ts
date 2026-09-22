d60666781c664aced87edf373fd968c69ca44495
Dmitry Zhukov <56310359+dmitryjima@users.noreply.github.com>
2026-09-21 22:18:50 -0500

    Enable sassDts plugin only for development mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import sassDts from 'vite-plugin-sass-dts';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import sassDts from "vite-plugin-sass-dts";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), sassDts()],
+  plugins: [
+    react(),
+    sassDts({
+      enabledMode: ["development"],
+    }),
+  ],
 });
