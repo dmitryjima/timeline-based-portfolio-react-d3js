3ef9d18feaf7e9a9c2827d53a76685d05671ddc1
Dmitry Zhukov <56310359+dmitryjima@users.noreply.github.com>
2026-09-23 11:32:33 -0500

    Implement Animating resize chapter

---
+++ src/8-animating-resize/lib/timeline/components/TimelineMap/styles.module.d.scss.ts
@@ -0,0 +1,13 @@
+declare const classNames: {
+  readonly "scroll-wrapper": "scroll-wrapper";
+  readonly "inner-content": "inner-content";
+  readonly "svg-map": "svg-map";
+  readonly baseline: "baseline";
+  readonly "month-tick": "month-tick";
+  readonly "year-tick": "year-tick";
+  readonly "year-label": "year-label";
+  readonly connector: "connector";
+  readonly commitment: "commitment";
+  readonly project: "project";
+};
+export = classNames;
